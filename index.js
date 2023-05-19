01daaa3b3c928c06491103ef0cd87ce188990e70
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-19 21:52:11 +0600

    shop category backend json file added

--- index.js
+++ index.js
@@ -15,24 +15,54 @@ const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const catagoryCollections = client.db('kidsPlace').collection('catagorys')
+
+        // app.get('/catagory/:text', async (req, res) => {
+        //     if (req.params.text == "car" || eq.params.text == "truck" || eq.params.text == "police") {
+        //         const cursor = catagoryCollections.find({ sub_category: req.params.text });
+        //         const result = await cursor.toArray()
+        //         res.send(result)
+        //     }
+        //     const cursor = catagoryCollections.find();
+        //     const result = await cursor.toArray()
+        //     res.send(result)
+
+
+        // })
+        app.get('/catagory/:text', async (req, res) => {
+            let cursor;
+            if (req.params.text === "car" || req.params.text === "truck" || req.params.text === "police") {
+                cursor = catagoryCollections.find({ sub_category: req.params.text });
+            } else {
+                cursor = catagoryCollections.find();
+            }
+
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -41,8 +71,8 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('car doctor is running')
+    res.send('Kids Place is running')
 })
 app.listen(port, () => {
-    console.log(`car doctor is  running on port ${port}`);
+    console.log(`Kids Place is  running on port ${port}`);
 })
